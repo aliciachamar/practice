982c8776076d762bd242f413b30bb3226600c9f3
aliciachamar <aliciachamar@gmail.com>
2021-05-01 12:53:04 -0600

    unhid app.js

---
+++ branching/app.js
@@ -0,0 +1,9 @@
+var chosenPet = "Lulu";
+
+var shelter = {
+  dogs: ["Mackie", "Bernice", "Cookie Monster", "Spot"],
+  cats: ["Astrid", "Lulu", "Fluffy", "Mouser"],
+  apptMessage: function () {
+    console.log("Email us at meetafriend@waywardpets.com to make an appointment to meet your new friend.");
+  }
+};
